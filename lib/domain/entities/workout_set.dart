66c11f432ec188520b3ba5088af18465c771e776
AliakseiYafremau <aleksey.yafremau@gmail.com>
2025-12-12 23:30:41 +0000

    Eleminate weight from plannedset

--- lib/domain/entities/workout_set.dart
+++ lib/domain/entities/workout_set.dart
@@ -35,29 +35,14 @@ class PlannedSet {
   final Id id;
   final Exercise exercise;
   final int targetRepetitions;
-  final double? targetWeight;
 
   PlannedSet({
     required this.id,
     required this.exercise,
     required this.targetRepetitions,
-    this.targetWeight,
   }) {
     if (targetRepetitions <= 0) {
       throw ArgumentError('Target repetitions must be greater than zero');
-    }
-    if (targetWeight != null && targetWeight! < 0) {
-      throw ArgumentError('Target weight cannot be negative');
-    }
-    if (!exercise.usesWeights && targetWeight != null && targetWeight! > 0) {
-      throw ArgumentError(
-        'Planned set for exercise "${exercise.name}" does not use weights, target weight must be null or zero',
-      );
-    }
-    if (exercise.usesWeights && targetWeight == null) {
-      throw ArgumentError(
-        'Planned set for exercise "${exercise.name}" uses weights, target weight must be provided',
-      );
     }
   }
 }
